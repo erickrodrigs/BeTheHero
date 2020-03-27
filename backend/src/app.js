ead91d3c6abae38974da7f93eec1a110f1cdb7aa
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2020-03-27 18:34:45 -0300

    Added tests and validations

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 /**
  * Rota / Recursos
  */
@@ -29,4 +31,4 @@ app.use(routes);
  * recursos
  */
 
-app.listen(3333);
+module.exports = app;
